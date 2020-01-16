b935d0b2c27feaa58988e9f8fd6c57afa31b581f
Ahmed Magdy <madimagdy@gmail.com>
2020-01-16 17:16:38 +0200

    config request with bodyparser

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,15 @@
 const path = require('path');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
+const bodyParser = require('body-parser');
+
+const urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+const jsonParser = bodyParser.json();
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 const AYLIENTextAPI = require('aylien_textapi');
 const textapi = new AYLIENTextAPI({
@@ -8,45 +17,38 @@ const textapi = new AYLIENTextAPI({
     application_key: process.env.API_KEY
 });
 
-const dotenv = require('dotenv');
-dotenv.config();
-
 const app = express();
 
 app.use(express.static('dist'));
 
 console.log(__dirname);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile('dist/index.html');
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(3030, function() {
+app.listen(3030, function () {
     console.log('Server running on port 3030!');
 });
 
-app.post('/add', (req, res) => {
+app.post('/add', jsonParser, (req, res) => {
     //console.log(req.body);
-    try {
-        console.log('trying to fetch...');
-        const sentiment = textapi.sentiment(
-            {
-                text: req.body.formText
-            },
-            function(error, response) {
-                if (error === null) {
-                    console.log(response);
-                    res.send(response);
-                }
+    console.log('trying to fetch...');
+    console.log(textapi);
+    console.log('the request body ==> ', req.body.formText);
+    textapi.sentiment({
+            text: req.body.formText
+        },
+        function (error, response) {
+            console.log(error);
+            if (error === null) {
+                res.send(response);
             }
-        );
-        console.log(sentiment);
-    } catch (error) {
-        console.log(error);
-    }
+        }
+    );
 });
 
-app.get('/test', function(req, res) {
+app.get('/test', function (req, res) {
     res.send(mockAPIResponse);
 });
